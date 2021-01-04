5f045579b8e8ce78b4d25c17a63396bafb0f9ca8
Nivas Muthu M G <nivasmuthu452@gmail.com>
2021-01-04 17:00:16 +0530

    defined getAuthAccessToken() to get accessToken from spotify

--- main.go
+++ main.go
@@ -1,5 +1,50 @@
 package main
 
+import (
+	"bytes"
+	"encoding/base64"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+)
+
+func getAppAuthAccessToken() (map[string]string, error) {
+
+	data := os.Getenv("CLIENT_ID") + ":" + os.Getenv("CLIENT_SECRET")
+	encodedClient := base64.StdEncoding.EncodeToString([]byte(data))
+	fmt.Println(encodedClient)
+
+	reqBody := []byte("grant_type=client_credentials")
+
+	req, err := http.NewRequest("POST", "https://accounts.spotify.com/api/token", bytes.NewBuffer(reqBody))
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+	req.Header.Add("Authorization", "Basic "+encodedClient)
+
+	client := http.Client{}
+
+	response, err := client.Do(req)
+	if err != nil {
+		return nil, err
+
+	}
+
+	body, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	parsedBody := make(map[string]string)
+	json.Unmarshal(body, &parsedBody)
+
+	return parsedBody, nil
+}
+
 func main() {
 
 }
